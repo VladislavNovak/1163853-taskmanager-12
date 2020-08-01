768bac44331d29e97a8f0c881b18a02485eb0683
Vladislav Novak <novaldix@gmail.com>
2020-08-01 22:21:54 +0300

    окончательно переименовывает папку view

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import {createSiteMenuTemplate} from "./_view/site-menu.js";
-import {createFilterTemplate} from "./_view/filter.js";
-import {createBoardTemplate} from "./_view/board.js";
-import {createTaskTemplate} from "./_view/task.js";
-import {createTaskEditTemplate} from "./_view/task-edit.js";
-import {createLoadMoreButtonTemplate} from "./_view/load-more-button.js";
+import {createSiteMenuTemplate} from "./view/site-menu.js";
+import {createFilterTemplate} from "./view/filter.js";
+import {createBoardTemplate} from "./view/board.js";
+import {createTaskTemplate} from "./view/task.js";
+import {createTaskEditTemplate} from "./view/task-edit.js";
+import {createLoadMoreButtonTemplate} from "./view/load-more-button.js";
 
 const TASK_COUNT = 3;
 
